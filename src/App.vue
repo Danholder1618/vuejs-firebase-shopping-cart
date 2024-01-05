8be5af11d53f33fa7ce9aa6b508e591725e74507
Danholder1618 <Danholder2003@yandex.com>
2024-01-05 06:45:47 +0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,77 @@
 <template>
-<div class="d-flex flex-column sticky-footer-wrapper">
-    <main class="flex-fill">
+  <div class="app-container">
+    <header>
       <app-header></app-header>
       <message-component></message-component>
-      <div class="container mt-3">
-          <div class="row">
-            <div class="col-md-12">
-              <router-view></router-view>
-              <div class="card panel-warning d-none d-sm-flex" id="reset-store-panel">
-                <div class="card-header">Admin Panel (Testing purpose)</div>
-                  <div class="card-body text-center">
-                    <button class="btn btn-warning">Reset Store</button>
-                  </div>
-              </div>
-            </div>
-          </div>
-      </div>
+    </header>
+
+    <main>
+      <router-view></router-view>
     </main>
 
-    <footer class="navbar-default navbar-bottom navbar-dark bg-dark">
-      <div class="container-fluid">
-            <p class="text-center nav-bar mb-0">Checkout repository at <a href="https://github.com/ittus/vuejs-firebase-shopping-cart" target="_blank">
-              https://github.com/ittus/vuejs-firebase-shopping-cart
-            </a></p>
+    <footer class="navbar-dark bg-primary text-white">
+      <div class="container">
+        <p class="text-center mb-0">
+          Explore our latest collection of laptops at
+          <a href="https://github.com/ittus/vuejs-firebase-shopping-cart" target="_blank" class="text-light">
+            Vue Laptop Store
+          </a>
+        </p>
       </div>
     </footer>
-</div>
-
+  </div>
 </template>
 
 <script>
-  import { mapActions } from 'vuex';
-  import Header from './components/Header.vue';
-  import MessageComponent from './components/common/MessageComponent.vue';
-  export default {
-    components: {
-      appHeader: Header,
-      MessageComponent
-    },
-    methods: {
-      ...mapActions(['getShoppingCart', 'listenToProductList'])
-    },
-    created() {
-      let uid = this.$store.getters.currentUser.uid;
-      this.listenToProductList();
-      this.getShoppingCart({uid, currentCart: this.$store.getters.cartItemList});
-    }
+import { mapActions } from 'vuex';
+import Header from './components/Header.vue';
+import MessageComponent from './components/common/MessageComponent.vue';
+
+export default {
+  components: {
+    appHeader: Header,
+    MessageComponent
+  },
+  methods: {
+    ...mapActions(['getShoppingCart', 'listenToProductList'])
+  },
+  created() {
+    let uid = this.$store.getters.currentUser.uid;
+    this.listenToProductList();
+    this.getShoppingCart({ uid, currentCart: this.$store.getters.cartItemList });
   }
+};
 </script>
 
 <style>
-  #reset-store-panel {
-    position: fixed; bottom: 0px; right: 0px;
-  }
+.app-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
 
-  body, .sticky-footer-wrapper {
-     min-height:100vh;
-  }
+header {
+  flex: 0 0 auto;
+}
 
-  .flex-fill {
-     flex:1 1 auto;
-  }
-  footer {
-    height: 40px;
-    color: #666;
-    padding: 10px 0 10px 0;
-    font-size: 85%;
-  }
-  footer a {
-    color: #999;
-  }
-  footer a:hover {
-    color: #efefef;
-  }
-  @media (max-width: 576px) {
-    footer {
-      height: 50px;
-    }
-  }
+main {
+  flex: 1 1 auto;
+  padding-top: 20px;
+}
+
+footer {
+  flex: 0 0 auto;
+  padding: 10px 0;
+  color: #666;
+  font-size: 85%;
+  background-color: #343a40;
+}
+
+footer a {
+  color: #999;
+}
+
+footer a:hover {
+  color: #efefef;
+}
 </style>
